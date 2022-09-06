558ace2714f7e680c54e07f137d86518935c0940
John Ferlito <johnf@inodes.org>
2022-09-07 09:54:32 +1000

    Add code push

--- android/app/src/main/java/com/interactions/MainApplication.java
+++ android/app/src/main/java/com/interactions/MainApplication.java
@@ -13,10 +13,17 @@ import com.interactions.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.microsoft.codepush.react.CodePush;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
